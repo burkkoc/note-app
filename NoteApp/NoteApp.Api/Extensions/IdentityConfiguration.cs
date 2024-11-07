25af85fad8e6da3524c99a8c82a4dc76594eee5b
burakk0c <burkkoc@gmail.com>
2024-11-07 17:49:51 +0300

    Revert "IdentityConfiguration güncellendi. Key'in null dönme durumu giderildi. JWTOptions sınıfı ile birlikte gerekli parametrelerin doğruluğu güçlendirildi."
    This reverts commit 36e604de0df1142b24deda529727280979708e3a.

--- NoteApp/NoteApp.Api/Extensions/IdentityConfiguration.cs
+++ NoteApp/NoteApp.Api/Extensions/IdentityConfiguration.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using NoteApp.DTOs.Authentication;
 using System.Text;
 
 namespace NoteApp.Api.Extensions
@@ -9,39 +8,23 @@ namespace NoteApp.Api.Extensions
     {
         public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
-            services.Configure<JWTOption>(configuration.GetSection("Jwt"));
-            var jwtOptions = configuration.GetSection("Jwt").Get<JWTOption>();
-            services.AddSingleton<TokenValidationParameters>(serviceProvider =>
+            services.AddAuthentication(options =>
             {
-                return new TokenValidationParameters
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuer = true,
                     ValidateAudience = true,
-                    ValidIssuer = jwtOptions.Issuer,
-                    ValidAudience = jwtOptions.Audience,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Key)),
-                    ValidateLifetime = true
-                };
-            });
-            services.AddAuthentication(opt =>
-            {
-                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-               
-            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
-            {
-                opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
-                {
-                    ValidIssuer = jwtOptions.Issuer,
-                    ValidAudience = jwtOptions.Audience,
-                    ValidateIssuerSigningKey = true,
-                    ValidateAudience = true, 
                     ValidateLifetime = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Key ?? throw new ArgumentException("Security key cannot be null or empty.", nameof(jwtOptions.Key)))),
-                    ClockSkew = TimeSpan.Zero
-                   
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
                 };
-
             });
 
             return services;
--- NoteApp/NoteApp.Business/Features/Members/Commands/CreateMemberHandler.cs
+++ NoteApp/NoteApp.Business/Features/Members/Commands/CreateMemberHandler.cs
@@ -1,8 +1,6 @@
 ﻿using AutoMapper;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using NoteApp.Business.Services;
 using NoteApp.DataAccessEFCore.Repositories;
 using NoteApp.DataAccessEFCore.Repositories.UserRepositories;
 using NoteApp.Entities.DbSets;
@@ -11,7 +9,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
-using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,38 +19,29 @@ namespace NoteApp.Business.Features.Users.Commands
         private readonly UserManager<IdentityUser> _userManager;
         private readonly MemberWriteRepository _writeRepository;
         private readonly IMapper _mapper;
-        private readonly PasswordService _passwordService;
-        private readonly JWTService _jwtService;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        public CreateMemberHandler(UserManager<IdentityUser> userManager, MemberWriteRepository writeRepository, PasswordService passwordService, IMapper mapper, JWTService jwtservice, IHttpContextAccessor httpContextAccessor)
+        public CreateMemberHandler(UserManager<IdentityUser> userManager, MemberWriteRepository writeRepository, IMapper mapper)
         {
             _userManager = userManager;
             _writeRepository = writeRepository;
             _mapper = mapper;
-            _passwordService = passwordService;
-            _jwtService = jwtservice;
-            _httpContextAccessor = httpContextAccessor;
         }
         public async Task<bool> Handle(CreateMemberCommand request, CancellationToken cancellationToken)
         {
-            var claims = _jwtService.ValidateToken(_httpContextAccessor);
-            var registererUserName = _jwtService.GetUsername(claims);
-            
             var identityUser = _mapper.Map<IdentityUser>(request);
             var member = _mapper.Map<Member>(request);
 
             member.IdentityId = identityUser.Id;
-            member.CreatedBy = registererUserName;
+            member.CreatedBy = "System";
 
             bool memberResult = await _writeRepository.AddAsync(member);
             if (!memberResult)
                 return false;
-            string pass = _passwordService.GenerateRandomPassword();
-            var identityResult = await _userManager.CreateAsync(identityUser, pass);
+
+            var identityResult = await _userManager.CreateAsync(identityUser);
             if (!identityResult.Succeeded)
                 throw new Exception("Member creation failed: " + string.Join(", ", identityResult.Errors.Select(e => e.Description)));
 
-           
+
 
             await _writeRepository.SaveAsync();
 
