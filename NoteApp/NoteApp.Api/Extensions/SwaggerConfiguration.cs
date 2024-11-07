70114ba202a0dbf26d170048addbcc0a09a39586
burakk0c <burkkoc@gmail.com>
2024-11-07 15:25:04 +0300

    authentication için identity ve swagger konfigürasyonları eklendi.

---
+++ NoteApp/NoteApp.Api/Extensions/SwaggerConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace NoteApp.Api.Extensions
+{
+    public static class SwaggerConfiguration
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            // JWT için Swagger konfigürasyonu tanımlandı.
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My Profile API", Version = "v1" });
+
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Authentication",
+                    Description = "Enter your JWT token",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                };
+
+                c.AddSecurityDefinition("Bearer", securityScheme);
+
+                var securityRequirement = new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] {}
+            }
+        };
+
+                c.AddSecurityRequirement(securityRequirement);
+            });
+
+            return services;
+        }
+    }
+}
