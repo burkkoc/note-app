1ae2cf2a4f70da95e72e5380d9bfb9ffb1dcf993
burakk0c <burkkoc@gmail.com>
2024-11-08 17:01:14 +0300

    Login işlemi için command, handler ve response oluşturuldu.

---
+++ NoteApp/NoteApp.Business/Features/Login/LoginHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using NoteApp.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp.Business.Features.Login
+{
+    public class LoginHandler : IRequestHandler<LoginCommand, LoginResponse>
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JWTService _jwtService;
+
+        public LoginHandler(UserManager<IdentityUser> userManager,  JWTService jwtService)
+        {
+            _userManager = userManager;
+            _jwtService = jwtService;
+        }
+        public async Task<LoginResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user != null && await _userManager.CheckPasswordAsync(user, request.Password))
+            {
+                var token = await _jwtService.GenerateTokenAsync(user);
+                return new LoginResponse(token);
+            }
+            
+            throw new UnauthorizedAccessException("Invalid credentials."); 
+        }
+    }
+}
---
+++ NoteApp/NoteApp.Business/Features/Login/LoginResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp.Business.Features.Login
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public LoginResponse(string token)
+        {
+            Token = token;
+        }
+    }
+}
