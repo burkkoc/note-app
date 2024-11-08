7816f98ba58de4bb2ea31be021502eeab403aa06
burakk0c <burkkoc@gmail.com>
2024-11-09 02:49:41 +0300

    GetNoteById eklendi.

---
+++ NoteApp/NoteApp.Business/Features/Notes/Queries/GetNoteById/GetNoteByIdHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using NoteApp.Business.Features.Users.Queries;
+using NoteApp.DTOs.Note;
+using NoteApp.DTOs.User;
+using NoteApp.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp.Business.Features.Notes.Queries.GetNoteById
+{
+    public class GetNoteByIdHandler : IRequestHandler<GetNoteByIdQuery, NoteDTO>
+    {
+        private readonly INoteReadRepository _noteReadRepository;
+        private readonly IMapper _mapper;
+        public GetNoteByIdHandler(INoteReadRepository noteReadRepository, IMapper mapper)
+        {
+            _noteReadRepository = noteReadRepository;
+            _mapper = mapper;
+        }
+        public async Task<NoteDTO> Handle(GetNoteByIdQuery request, CancellationToken cancellationToken)
+        {
+            var note = await _noteReadRepository.GetByIdAsync(request.Id.ToString());
+            if (note == null) throw new Exception("Note not found.");
+            return _mapper.Map<NoteDTO>(note);
+        }
+    }
+}
---
+++ NoteApp/NoteApp.Business/Features/Notes/Queries/GetNoteById/GetNoteByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using NoteApp.DTOs.Note;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp.Business.Features.Notes.Queries.GetNoteById
+{
+    public class GetNoteByIdQuery : IRequest<NoteDTO>
+    {
+        public Guid Id { get; }
+        public GetNoteByIdQuery(Guid noteId)
+        {
+            Id = noteId;
+        }
+    }
+}
