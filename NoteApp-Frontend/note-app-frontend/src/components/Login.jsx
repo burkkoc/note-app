be82b88d3f983e594c1439fa2db1dce91d313ef9
burakk0c <burkkoc@gmail.com>
2024-11-09 13:27:39 +0300

    Login ekranı tasarımı yapıldı.

--- NoteApp-Frontend/note-app-frontend/src/components/Login.jsx
+++ NoteApp-Frontend/note-app-frontend/src/components/Login.jsx
@@ -1,68 +1,122 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginUser } from '../store/authSlice';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../assets/styles/LoginForm.scss";
+import { faEye, faEyeSlash, faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+
 
 const Login = () => {
   const dispatch = useDispatch();
   const { loading, error } = useSelector((state) => state.auth);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+    credentials: '',
+    hasErrors: false
+  });
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setEmail(e.target[0].value);
+    setPassword(e.target[1].value);
 
-    const newErrors = {};
-    if (!email) {
-      newErrors.email = 'Email alanı zorunludur.';
-    }
-    if (!password) {
-      newErrors.password = 'Şifre alanı zorunludur.';
-    }
 
-    if (Object.keys(newErrors).length > 0) {
-      setErrors(newErrors);
+    validationControl(email, password);
+    
+    if (errors.hasErrors) {
+      console.log(errors);
       return;
     }
-
-    // Veriyi dispatch ile Redux thunk fonksiyonuna gönderiyoruz
+    console.log(email,password);
+  
     dispatch(loginUser({ email, password }));
   };
+  const validationControl = (email, password) => {
+    const newErrors = {
+      email: '',
+      password: '',
+      credentials: '',
+      hasErrors:false
+    };
+
+    if (!email) {
+         newErrors.email ="Email cannot be empty.";
+         newErrors.hasErrors=true;
+     }
+    if (!password) {
+     newErrors.password =  "Password cannot be empty.";
+     newErrors.hasErrors=true;
+    }
+
+    if (newErrors.hasErrors) {
+      setErrors(newErrors);
+    } else {
+      setErrors({
+        email: '',
+        password: '',
+        credentials: '',
+        hasErrors:false
+      });
+    }
+   
+};
+
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-            setErrors({ ...errors, email: '' });
-          }}
-          required
-        />
-        {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
 
-        <input
-          type="password"
-          placeholder="Şifre"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-            setErrors({ ...errors, password: '' });
-          }}
-          required
-        />
-        {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
+    <div className="login-container">
+      <div className="cont">
+        <div className="login__cont">
+          <div className="login">
+            <div className="login__check"></div>
+            <div className="login__form">
+              <form onSubmit={handleSubmit} className="form-login">
+                <div className="login__row">
+                  <FontAwesomeIcon icon={faUser} className="login__icon" />
+                  <input
+                    type="text"
+                    className="login__input name"
+                    name="email"
+                    placeholder="Email"
+                  />
+                </div>
+                {errors.email != ''&& <p className="error-message">{errors.email}</p>}
 
-        <button type="submit" disabled={loading}>
-          {loading ? 'Giriş Yapılıyor...' : 'Giriş Yap'}
-        </button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-      </form>
+                <div className="login__row">
+                  <FontAwesomeIcon icon={faLock} className="login__icon" />
+                  <div className="input-password">
+                    <input
+                      type="password"
+                      className="login__input pass"
+                      name="password"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
+
+
+                 {errors.password != '' && <p className="error-message">{errors.password}</p>}
+                 {/* {(!errors.password === '' && !errors.email === '') && errors.credentials && (<p className="error-message">{errors.credentials}</p>)} */}
+                <button
+                  type="submit"
+                  className="login__submit"
+                  >
+                    Login
+                </button>
+                <p className="login__resendpassword">
+                  &nbsp;<a href="#">Forgot Password? Re-send.</a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    
   );
 };
 
