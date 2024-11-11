3fe1b5b9d11b1b96dd5a3125631daee06258eb78
burakk0c <burkkoc@gmail.com>
2024-11-11 16:24:34 +0300

    tokenDecoder eklendi.

--- NoteApp-Frontend/note-app-frontend/src/components/HomeComp.jsx
+++ NoteApp-Frontend/note-app-frontend/src/components/HomeComp.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import 'flowbite/dist/flowbite.css';
 const Home = () => {
-    const {user} = useSelector(state => state.auth);
+    const {user, claims} = useSelector(state => state.auth);
 
 return (
     <div className="home-comp-cont">
       
       <div className="flex-1 p-4">
         <h1>Hello {user.firstName}!</h1>
+        {claims ? (
+          
+          <ul>
+          {Object.entries(claims).map(([key, value]) => (
+            <li key={key}>
+              <strong>{key}:</strong> {value.toString()}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Claim bilgileri yükleniyor...</p>
+      )}
         <p>This is your landing page.</p>
       </div>
     </div>
--- NoteApp-Frontend/note-app-frontend/src/redux/slices/authSlice.js
+++ NoteApp-Frontend/note-app-frontend/src/redux/slices/authSlice.js
@@ -1,12 +1,32 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import api from '../../Api/api';
 
+
+
+
+function decodeTokenFunc(token) {
+  if(token && token !== undefined){
+    console.log(token);
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+      .split('')
+      .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+      .join('')
+    );
+    return JSON.parse(jsonPayload);
+  }
+}
+
 export const loginUser = createAsyncThunk(
   'auth/loginUser',
-  async ({ email, password }, { rejectWithValue }) => {
+  async ({ email, password }, {dispatch, rejectWithValue }) => {
     try {
       const response = await api.post('/Authentication/Login', { email, password });
       if (response.status === 200) {
+        dispatch(decodeToken(response.data.token || null));
+        
         return response.data; 
       }
       return rejectWithValue('Login failed');
@@ -16,11 +36,25 @@ export const loginUser = createAsyncThunk(
   }
 );
 
+export const decodeToken = createAsyncThunk(
+  'auth/decodeToken',
+  async (token, { rejectWithValue }) => {
+    try {
+      const claims = decodeTokenFunc(token);
+      return claims;
+    } catch (error) {
+      console.error("Decode işlemi sırasında hata: ", error);
+      return rejectWithValue('Token geçersiz veya decode edilemedi');
+    }
+  }
+);
+
 const authSlice = createSlice({
   name: 'auth',
   initialState: {
     user:  JSON.parse(localStorage.getItem('user')) || null,
     token: localStorage.getItem('token') || null, 
+    claims: null,
     loading: false,
     error: null,
   },
@@ -28,6 +62,7 @@ const authSlice = createSlice({
     logout: (state) => {
       state.user = null;
       state.token = null;
+      state.claims = null;
       localStorage.removeItem('user');
       localStorage.removeItem('token'); 
     },
@@ -44,9 +79,17 @@ const authSlice = createSlice({
         state.token = action.payload.token;
         localStorage.setItem('user', JSON.stringify(action.payload.memberDTO));
         localStorage.setItem('token', action.payload.token);
+
       })
       .addCase(loginUser.rejected, (state, action) => {
         state.loading = false;
+        state.error = action.payload;
+      });
+      builder
+      .addCase(decodeToken.fulfilled, (state, action) => {
+        state.claims = action.payload;
+      })
+      .addCase(decodeToken.rejected, (state, action) => {
         state.error = action.payload;
       });
   },
